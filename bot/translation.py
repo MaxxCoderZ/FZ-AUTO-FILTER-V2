ea75088b71d0ae859ae9b805aa0dff35d68df3e3
Mᴀxx ⚡ <84794903+MaxxCoderZ@users.noreply.github.com>
2021-06-01 10:24:19 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -6,10 +6,10 @@ class Translation(object):
     
       
     START_TEXT = """<b>Hey {}</b>
-ɪ ᴀᴍ ᴀ ᴀᴜᴛᴏ ғɪʟᴛᴇʀ ʙᴏᴛ sɪᴍᴘʟʏ sᴇᴀʀᴄʜ ғᴏʀ ᴛʜᴇ ғɪʟᴇs ᴍᴀᴅᴇ ғᴏʀ <b><a href="https://t.me/Film_zone_group">Film Zone</a></b>
+ɪ ᴀᴍ ᴀ ᴀᴜᴛᴏ ғɪʟᴛᴇʀ ʙᴏᴛ sɪᴍᴘʟʏ sᴇᴀʀᴄʜ ғᴏʀ ᴛʜᴇ ғɪʟᴇs ᴍᴀᴅᴇ ғᴏʀ <b><a href="https://t.me/Film_zone_group">Mallu Villa</a></b>
 ᴀᴄᴄᴏʀᴅɪɴɢ ᴛᴏ ɢɪᴠᴇɴ ᴏ̨ᴜᴇʀʏ ᴀɴᴅ ɢɪᴠᴇs ʟɪɴᴋ ᴛᴏ ᴛʜᴏsᴇ ғɪʟᴇs ᴀs ʙᴜᴛᴛᴏɴs
 
-🕵‍♂ ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ 👉 <b><a href="https://t.me/diago_x">Diago</a></b>
+🕵‍♂ ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ 👉 <b><a href="https://t.me/MaxxCoderz">Màx</a></b>
 """    
     
     HELP_TEXT = """
@@ -45,13 +45,13 @@ class Translation(object):
 
 """
     
-    ABOUT_TEXT = """○ ᴍʏ ɴᴀᴍᴇ: Magna
+    ABOUT_TEXT = """○ ᴍʏ ɴᴀᴍᴇ: Radhika Bot
     
 ○ ʟᴀɴɢᴜᴀɢᴇ  : ᴘʏᴛʜᴏɴ
 
 ○ ғʀᴀᴍᴇᴡᴏʀᴋ  : ᴘʏʀᴏɢʀᴀᴍ
 
-○ ᴄʀᴇᴀᴛᴏʀ     : <a href="https://t.me/diago_x">Diago</a>
+○ ᴄʀᴇᴀᴛᴏʀ     : <a href="https://t.me/Mãxx">Diago</a>
 
-○ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ : <a href="https://github.com/ajmalyaseen/fz-auto-filter-bot-v2">Click Hear</a>
+○ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ : <a href="https://github.com/MaxxCoderz/fz-auto-filter-bot-v2">Click Here</a>
 """
